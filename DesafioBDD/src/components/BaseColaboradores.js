75b48b934d3f880143ae175d5631befef86fc335
mportilla <mothaccounts@gmail.com>
2023-09-12 11:21:58 -0300

    Creación componentes y orden general

---
+++ DesafioBDD/src/components/BaseColaboradores.js
@@ -0,0 +1,26 @@
+export const BaseColaboradores = [
+    {
+        id: "1",
+        nombre: "Juan Soto",
+        correo: "juan@colaborador.com",
+        edad: "23",
+        cargo: "Desarrollador FrontEnd",
+        telefono: "99887766"
+    },
+    {
+        id: "2",
+        nombre: "Lucas Paimilla",
+        correo: "lucasp@colaborador.com",
+        edad: "31",
+        cargo: "Desarrollador BackEnd",
+        telefono: "88779955"
+    },
+    {
+        id: "3",
+        nombre: "DIego Riquelme",
+        correo: "diegor@colaborador.com",
+        edad: "28",
+        cargo: "Desarrollador DevOps",
+        telefono: "99226644"
+    },
+]
