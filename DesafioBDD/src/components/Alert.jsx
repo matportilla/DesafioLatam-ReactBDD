9a089d22d47aa6564644aa07f509c8b4c9fa045e
Nicoarratia <nicolas.andres.arratia@gmail.com>
2023-09-12 11:58:24 -0300

    agrega componente alert, succes/warning

---
+++ DesafioBDD/src/components/Alert.jsx
@@ -0,0 +1,9 @@
+const Alert = ({ color, children }) => {
+    return (
+        <>
+            <div className={`alert alert-${color} my-2`}>{children}</div>
+        </>
+    )
+}
+
+export default Alert;
